2c033ee4d2fa09f58b4baf46d610803bd29cbed5
Sephie <sp223tj@student.lnu.se>
2026-09-07 23:41:26 +0200

    Create basic input with response using readline.

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log("Hello, World!");
+const readline = require('readline')
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+rl.question('Vad heter du? ', (namn) => {
+    console.log(`Hej ${namn}!`)
+    rl.close()
+})
