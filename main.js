23139699bcf5e225608025dff289ae4182b72062
Sephie <sp223tj@student.lnu.se>
2026-09-08 01:14:21 +0200

    Add more quotes and create separate file for them.

--- main.js
+++ main.js
@@ -1,23 +1,10 @@
 const readline = require('readline')
+const quotes = require('./quotes')
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 })
-
-const quotes = [
-    "It's dangerous to go alone! Take this. - Old man, The legend of Zelda",
-    "It's a-me, Mario! - Mario, Super Mario 64",
-    "Do you think love can bloom even on a battlefield? - Otacon, Metal Gear Solid",
-    "What is a man? A miserable little pile of secrets. - Dracula, Castlevania: Symphony of the Night",
-    "The cake is a lie. - Wall graffiti, Portal",
-    "All your base are belong to us! - Cats, Zero Wing",
-    "When you're lost in the darkness, look for the light. - Fireflies, The Last of Us",
-    "Hey! Listen! - Navi, The legend of Zelda: Ocarina of Time", 
-    "Wind's howling. - Geralt, The Witcher 3: Wild Hunt",
-    "For those who come after. - Motto, Clair Obscur: Expedition 33" ,
-    "Did I ever tell you what the definition of insanity is? - Vaas, Far Cry 3" 
-]
 
 rl.question('Vad heter du? ', (namn) => {
     const response = `You've met with a terrible fate, haven't you, ${namn}?`
---
+++ quotes.js
@@ -0,0 +1,20 @@
+const quotes = [
+    "'It's dangerous to go alone! Take this.' - Old man, The legend of Zelda",
+    "'It's a-me, Mario!' - Mario, Super Mario 64",
+    "'Do you think love can bloom even on a battlefield?' - Otacon, Metal Gear Solid",
+    "'What is a man? A miserable little pile of secrets.' - Dracula, Castlevania: Symphony of the Night",
+    "'The cake is a lie.' - Wall graffiti, Portal",
+    "'All your base are belong to us!' - Cats, Zero Wing",
+    "'When you're lost in the darkness, look for the light.' - Fireflies, The Last of Us",
+    "'Hey! Listen!' - Navi, The legend of Zelda: Ocarina of Time", 
+    "'Wind's howling.' - Geralt, The Witcher 3: Wild Hunt",
+    "'For those who come after.' - Motto, Clair Obscur: Expedition 33" ,
+    "'Did I ever tell you what the definition of insanity is?' - Vaas, Far Cry 3",
+    "'Finish him.' - Narrator, Mortal Kombat",
+    "'The right man in the wrong place can make all the difference in the world.' - G-Man, Half-Life 2",
+    "'I used to be an adventurer like you. Then I took an arrow in the knee.' - Town guard, Skyrim",
+    "'Stay awhile, and listen.' - Deckard Cain, Diablo",
+    "'Metal... Gear...?' - Solid Snake, Metal Gear Solid"
+]
+
+module.exports = quotes
